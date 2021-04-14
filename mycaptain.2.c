176da0c2d7450b704eb527cfd7ef2ce90ad9fdae
arti555 <82434394+arti555@users.noreply.github.com>
2021-04-14 12:28:02 +0530

    Add files via upload

---
+++ mycaptain.2.c
@@ -0,0 +1,69 @@
+#include <stdio.h> 
+
+int main(){
+    int marks;
+ 
+
+    printf("enter your english marks \n");
+scanf("%d", &marks);
+if (marks>=90 && marks<=100)
+printf("GRADE A \n");
+if (marks>=75 && marks<=89)
+printf("GRADE B\n");
+if (marks>=50 && marks<=74)
+printf("GRADE C\n");
+if (marks>=33 && marks<=49)
+printf("GRADE D\n");
+if (marks>=1 && marks<=32)
+printf("GRADE E FAIL\n");
+printf("enter your MATHS marks \n");
+scanf("%d", &marks);
+if (marks>=90 && marks<=100)
+printf("GRADE A \n");
+if (marks>=75 && marks<=89)
+printf("GRADE B\n");
+if (marks>=50 && marks<=74)
+printf("GRADE C\n");
+if (marks>=33 && marks<=49)
+printf("GRADE D\n");
+if (marks>=1 && marks<=32)
+printf("GRADE E FAIL\n");
+printf("enter your science marks \n");
+scanf("%d", &marks);
+if (marks>=90 && marks<=100)
+printf("GRADE A \n");
+if (marks>=75 && marks<=89)
+printf("GRADE B\n");
+if (marks>=50 && marks<=74)
+printf("GRADE C\n");
+if (marks>=33 && marks<=49)
+printf("GRADE D\n");
+if (marks>=1 && marks<=32)
+printf("GRADE E FAIL\n");
+printf("enter your social science marks \n");
+scanf("%d", &marks);
+if (marks>=90 && marks<=100)
+printf("GRADE A \n");
+if (marks>=75 && marks<=89)
+printf("GRADE B\n");
+if (marks>=50 && marks<=74)
+printf("GRADE C\n");
+if (marks>=33 && marks<=49)
+printf("GRADE D\n");
+if (marks>=1 && marks<=32)
+printf("GRADE E FAIL\n");
+printf("enter your hindi \n");
+scanf("%d", &marks);
+if (marks>=90 && marks<=100)
+printf("GRADE A \n");
+if (marks>=75 && marks<=89)
+printf("GRADE B\n");
+if (marks>=50 && marks<=74)
+printf("GRADE C\n");
+if (marks>=33 && marks<=49)
+printf("GRADE D\n");
+if (marks>=1 && marks<=32)
+printf("GRADE E FAIL\n");
+
+   return 0;
+}
